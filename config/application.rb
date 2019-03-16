765155fe9efb45239b3940ed53c883899e0759e8
senay <senaygeletaw@gmail.com>
2019-03-16 14:17:27 +0300

    added activeadmin css and js file to asset path

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,14 @@ module Hisab
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+        # Disable application initialize on precompile (heroku faq: fixes database access error on precompile)
+    config.assets.initialize_on_precompile = false
 
+    # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+    config.assets.precompile += %w(active_admin.css active_admin/print.css active_admin.js )
+
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
