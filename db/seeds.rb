88f87f813058a5edd50491e933498f9d5832a0d5
senay <senaygeletaw@gmail.com>
2019-03-16 13:30:18 +0300

    add cloudinary.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(full_name: "#{ENV['ADMIN_FULLNAME']}", email: "#{ENV['ADMIN_EMAIL']}", password: "#{ENV['SEED_PASSWORD']}", password_confirmation: "#{ENV['SEED_PASSWORD']}", role: "#{ENV['ROLE']}",company_name: "Yesu IT Solutions") if Rails.env.development?
+
+
+AdminUser.create!(full_name: "#{Rails.application.credentials.production[:full_name]}", 
+									email: "#{Rails.application.credentials.production[:admin_email]}", 
+									password: "#{Rails.application.credentials.production[:seed_password]}",password_confirmation: "#{Rails.application.credentials.production[:seed_password]}", 
+									company_name: "#{Rails.application.credentials.production[:company_name]}", 
+									tax_type: "#{Rails.application.credentials.production[:tax_type]}", 
+									role: "#{Rails.application.credentials.production[:role]}") if Rails.env.production?
