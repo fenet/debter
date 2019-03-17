49db2c248180d74191b4755987179bed8678d3c0
senay <senaygeletaw@gmail.com>
2019-03-17 13:08:37 +0300

    changed module name

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ end
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module InventoryManagement
+module Hisab
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
